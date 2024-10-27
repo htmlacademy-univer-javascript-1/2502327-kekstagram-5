87652e4427122ac4e6a3575bdaa4fd219344c58f
Keks <keks@htmlacademy.ru>
2024-10-27 19:58:53 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+import { generatePhotosArray } from './photos.js';
+
+const photos = generatePhotosArray();
+console.log(photos);
+
+// Здесь можно добавить дополнительный код для работы с фотографиями и отображения их на странице
