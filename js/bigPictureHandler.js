24ab874b421061fd83e81e536ecdb60dd308e485
Egor <egor_k2005@mail.ru>
2024-12-09 23:11:39 +0500

    8.14_6

--- js/bigPictureHandler.js
+++ js/bigPictureHandler.js
@@ -1,54 +1,53 @@
-const modal = document.querySelector('.big-picture');
+const bigPicture = document.querySelector('.big-picture');
 const bodyElement = document.body;
 
-const closeModal = () => {
-  modal.classList.add('hidden');
-  bodyElement.classList.remove('modal-open');
-  document.removeEventListener('keydown', onEscKeyPress);
-};
-
-const onEscKeyPress = (evt) => {
-  if (evt.key === 'Escape') {
-    closeModal();
-  }
-};
-
-const displayBigImage = (photoData) => {
-  const imageElement = modal.querySelector('.big-picture__img img');
-  const descriptionElement = modal.querySelector('.social__caption');
-  const likesElement = modal.querySelector('.likes-count');
-  const commentsElement = modal.querySelector('.comments-count');
-  const commentsContainer = modal.querySelector('.social__comments');
-  const commentCounter = modal.querySelector('.social__comment-count');
-  const loadMoreButton = modal.querySelector('.comments-loader');
+const displayBigPicture = function (pictureData) {
+  const imgElement = bigPicture.querySelector('.big-picture__img img');
+  const captionElement = bigPicture.querySelector('.social__caption');
+  const likesElement = bigPicture.querySelector('.likes-count');
+  const commentsElement = bigPicture.querySelector('.comments-count');
+  const commentsContainer = bigPicture.querySelector('.social__comments');
+  const commentCounter = bigPicture.querySelector('.social__comment-count');
+  const loadMoreButton = bigPicture.querySelector('.comments-loader');
 
   commentCounter.classList.add('hidden');
   loadMoreButton.classList.add('hidden');
-  imageElement.src = photoData.url;
-  imageElement.alt = photoData.description;
-  descriptionElement.textContent = photoData.description;
-  likesElement.textContent = `${photoData.likes}`;
-  commentsElement.textContent = `${photoData.comments.length}`;
+  imgElement.src = pictureData.url;
+  imgElement.alt = pictureData.description;
+  captionElement.textContent = pictureData.description;
+  likesElement.textContent = `${pictureData.likes}`;
+  commentsElement.textContent = `${pictureData.comments.length}`;
 
   commentsContainer.innerHTML = '';
 
-  photoData.comments.forEach((comment) => {
-    const commentElement = document.createElement('li');
-    commentElement.className = 'social__comment';
-    commentElement.innerHTML = `
-      <img class="social__picture"
-        src="${comment.avatar}"
-        alt="${comment.name}"
-        width="35" height="35">
+  pictureData.comments.forEach((comment) => {
+    const commentItem = document.createElement('li');
+    commentItem.className = 'social__comment';
+    commentItem.innerHTML = `<img class="social__picture"
+      src="${comment.avatar}"
+      alt="${comment.name}"
+      width="35" height="35">
       <p class="social__text">${comment.message}</p>`;
-    commentsContainer.appendChild(commentElement);
+    commentsContainer.appendChild(commentItem);
   });
 
-  modal.classList.remove('hidden');
+  bigPicture.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
 
-  document.addEventListener('keydown', onEscKeyPress);
-  modal.querySelector('.big-picture__cancel').addEventListener('click', closeModal);
+  const handleEscapePress = (evt) => {
+    if (evt.keyCode === 27) {
+      closePictureModal();
+    }
+  };
+
+  const closePictureModal = () => {
+    bigPicture.classList.add('hidden');
+    bodyElement.classList.remove('modal-open');
+    document.removeEventListener('keydown', handleEscapePress);
+  };
+
+  document.addEventListener('keydown', handleEscapePress);
+  bigPicture.querySelector('.big-picture__cancel').addEventListener('click', closePictureModal);
 };
 
-export { displayBigImage };
+export {displayBigPicture};
