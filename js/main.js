f8e507c2af61e887e435549aef6237a9d1dd4c12
Egor <egor_k2005@mail.ru>
2025-01-06 19:31:24 +0500

    final10

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { getData } from './photo-api.js';
 import { displayThumbnails } from './photo-thumbnails.js';
 import { initForm } from './form-init.js';
@@ -96,28 +95,3 @@ loadImages();
 
 // Инициализируем форму редактирования
 initForm(photosArray, displayThumbnails);
-=======
-import './util.js';
-import { generatePhotosArray } from './photos.js';
-import { displayThumbnails } from './pictureRenderer.js';
-import { displayBigImage } from './bigPictureHandler.js';
-
-// Генерация данных
-const photos = generatePhotosArray();
-
-// Отображение миниатюр
-displayThumbnails(photos);
-
-// Добавление обработчиков для открытия полноразмерного изображения
-const picturesContainer = document.querySelector('.pictures');
-picturesContainer.addEventListener('click', (event) => {
-  const pictureElement = event.target.closest('.picture');
-  if (pictureElement) {
-    const postId = pictureElement.getAttribute('data-id');
-    const post = photos.find((photo) => photo.id === Number(postId));
-    if (post) {
-      displayBigImage(post);
-    }
-  }
-});
->>>>>>> upstream/master
